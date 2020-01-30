bf93a05c514c91876382b1dae2b0c161bb28c55b
Paulo <paulofelipperp.dev@gmail.com>
2020-01-30 17:50:16 -0300

    Created get request to list all projects in the array

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ server.use(express.json());
 
 const projects = [];
 
+/* Add projects in the array (params: id, title) */
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
@@ -13,6 +14,11 @@ server.post('/projects', (req, res) => {
 
   projects.push(project);
 
+  return res.json(projects);
+});
+
+/* List all projects in the array */
+server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
